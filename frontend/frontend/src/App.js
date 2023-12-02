721489c8b3e5a387631f4142ab780d495d72b82e
Bashshar Atif <114458228+batif1@users.noreply.github.com>
2023-12-02 15:58:40 -0500

    implemented logging out

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -4,14 +4,16 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home.js'
 import Login from './pages/Login.js';
 import Signup from './pages/Signup.js';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
       <div className ="pages">
         <Routes>
-          <Route path="/home" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
         </Routes>
